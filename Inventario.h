f96abb995ee66829555a45420afea90cbaa7eb62
Skuviduvi <83040898+Skuviduvi@users.noreply.github.com>
2021-05-01 10:46:11 -0500

    Update Inventario.h

--- Inventario.h
+++ Inventario.h
@@ -1,25 +1,14 @@
 #ifndef Inventario_h
 #define Inventario_h
 #include <iostream>
-#include "Cliente.h"
 #include "Empleado.h"
 using namespace std;
 
-class Sucursal{
-  private:
-    string nombreSucursal;
-    int *inv, sizeMem, size;
-    Cliente cliente;
-    Empleado *empleado;
-  public:
-    Sucursal();
-    Sucursal(string);
-    string getnombreSucursal();
-    void agregarProducto(int,int);
-    void modificarProducto(int, int);
-    void eliminarProducto(int);
-    void aniadirEmpleado(Empleado);
-    void transferirEmpleado(Sucursal);
-    void eliminarEmpleado(int);
+class Inventario{
+    private:
+    public:
+        void añadir();
+        void eliminar();
+        void generarOrden(Empleado);
 };
 #endif
