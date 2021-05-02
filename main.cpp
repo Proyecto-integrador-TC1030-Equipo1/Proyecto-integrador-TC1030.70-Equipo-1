f432ebe095bbe4308368b47867b0bca8aaa7ea49
Skuviduvi <83040898+Skuviduvi@users.noreply.github.com>
2021-05-01 22:57:33 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -51,8 +51,17 @@ int main(){
     s2.aniadirEmpleado(g2);
     s2.aniadirEmpleado(v2);
     s2.aniadirEmpleado(a2);
-    
-
-
+    Producto p1 = Producto("Coca", 0, .500, 1, 3.5, 4.5);
+    Producto p2 = Producto("Cigars", 1, .05, 1, 3.0, 4,0);
+    Producto p3 = Producto("Papas", 2, .200, 1, 5.0, 6.0);
+    Producto p4 = Producto("Gansito", 3, .100, 1, 3.5, 4.5);
+    inv.agregarProducto(p1);
+    inv.agregarProducto(p2);
+    inv.agregarProducto(p3);
+    inv.agregarProducto(p4);
+    p1.setPeso(.600);
+    p1.setPrecioCompra(6.5);
+    p1.setPrecioCompra(7.5);
+    inv.eliminarProducto(2);
     return 0;
 }
